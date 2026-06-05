2bb867db4cd033dcfbe2e8c5c717b1682643e76c
armandor123 <armando.r123@hotmail.com>
2026-06-05 00:07:40 -0300

    chore: setup inicial do projeto spring boot

---
+++ minibanco/src/test/java/com/minibanco/minibanco/MinibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minibanco.minibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
